8f8a49aa9af28f1215af10843752a0b61ab8e0bb
GeroWalther <gero.walther@gmail.com>
2022-12-19 11:44:39 +0100

    font fix

--- components/policy/policy.js
+++ components/policy/policy.js
@@ -7,7 +7,8 @@ export default function Policy() {
     <div className={classes.policy}>
       <h1>プライバシーポリシー</h1>
       <div className={classes.d}>
-        皆様の個人情報の重要性を認識し、その適正な収集、利用、保護をはかるとともに、安全管理を行うため、プライバシーポリシーを定め、次のとおり運用します。
+        皆様の個人情報の重要性を認識し、その適正な収集、利用、保護をはかるとともに、安全管理を行うため、プライバシーポリシーを定め、
+        次のとおり運用します。
       </div>
       <Card className={classes.textcon}>
         <h2>1 - 個人情報の定義</h2>
