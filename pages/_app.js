bdbeec1d28881deaaf41480757d7c27b07c30c6f
GeroWalther <gero.walther@gmail.com>
2022-12-19 10:34:48 +0100

    keywords

--- pages/_app.js
+++ pages/_app.js
@@ -14,7 +14,11 @@ function MyApp({ Component, pageProps }) {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta
           name="keywords"
-          content="Web小説, ネット小説,携帯小説,ケータイ小説,オンライン小説,縦書き小説,小説投稿,小説投稿サイト"
+          content="Web小説,ネット小説,携帯小説,ケータイ小説,オンライン小説,縦書き小説,小説投稿,小説投稿サイト"
+        ></meta>
+        <meta
+          name="keywords"
+          content="艶っぽいブログ,艶っぽい,ブログ,ero-blog,ero,blog,japanese,novel,日常,冒険,私小説,騎士,エロい,びしょびしょ,濡れる,濡らす,ロマンチック,物語,小説,色っぽい,官能小説,えちえち,エッチ"
         ></meta>
         <meta property="og:image" content={prevImage} key="ogimage" />
       </Head>
