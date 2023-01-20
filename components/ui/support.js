376b0b439e48411b6f996386922eb315e5d47a00
GeroWalther <gero.walther@gmail.com>
2023-01-21 00:21:37 +0100

    mail

--- components/ui/support.js
+++ components/ui/support.js
@@ -8,7 +8,7 @@ export default function Support() {
       <h3>サポート</h3>
       <p>
         もちろん、外国人で、素人の僕には至らぬ点があるので、あくまで自分なりに書き上げた小説なので、もし文法的に合っていないところ、またはもっと艶っぽさが感じられるように書ければ訂正して、
-        個人的なメール: gero.walther@gmail.com または
+        個人的なメール: ero_blog@yahoo.com または
         <Link href="/contact">問い合わせ</Link>
         でご連絡をお待ちします。
       </p>
